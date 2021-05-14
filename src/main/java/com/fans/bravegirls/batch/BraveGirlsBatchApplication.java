6c7e4c4ad50b4ad902250561598f471acba98541
Red-Jung <applered80@gmail.com>
2021-05-14 13:12:28 +0900

    swappger 추가 maximum-pool-size 수정

--- src/main/java/com/fans/bravegirls/batch/BraveGirlsBatchApplication.java
+++ src/main/java/com/fans/bravegirls/batch/BraveGirlsBatchApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @EnableScheduling
 @SpringBootApplication
 public class BraveGirlsBatchApplication {
