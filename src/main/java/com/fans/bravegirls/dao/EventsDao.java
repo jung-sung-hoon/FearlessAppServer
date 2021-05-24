45a474f8c8fa0d36f9ce00f8e1228f0726045918
Red-Jung <applered80@gmail.com>
2021-05-25 01:10:38 +0900

    이벤트 D-1 인 경우 메시지 보내기 이벤트 지난것 false 로 변경

--- src/main/java/com/fans/bravegirls/dao/EventsDao.java
+++ src/main/java/com/fans/bravegirls/dao/EventsDao.java
@@ -14,4 +14,12 @@ public interface EventsDao {
 
     //이벤트 조회
     List<EventsVo> selectEventsInProgress();
+    
+    
+    //이벤트 D-1 , 오전 10시에 알람 보낸다.
+    List<EventsVo> selectEventDeadline(String endTime);
+    
+    
+    //00 시에 이벤트 지간것들 is_in_progress false 로 변경
+    int updateEventEnd(String endTime);
 }
--- src/main/java/com/fans/bravegirls/vo/code/SnsKind.java
+++ src/main/java/com/fans/bravegirls/vo/code/SnsKind.java
@@ -3,6 +3,9 @@ package com.fans.bravegirls.vo.code;
 public enum SnsKind {
     instagram
     ,twitter
+    ,event_dead_line
+    ,event_new
+    ,dc_noti
    
 }
 
