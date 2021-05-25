b7b6f185cfef0481a6b266ddfa28440eb5892b46
sungbum <shystyle@gmail.com>
2021-05-25 17:23:29 +0900

    feat : prod 환경에서만 스케쥴러 돌도록 수정

---
+++ src/main/java/com/fans/bravegirls/config/ScheduleProdConfig.java
@@ -0,0 +1,10 @@
+package com.fans.bravegirls.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+@Profile("prod")
+public class ScheduleProdConfig {}
