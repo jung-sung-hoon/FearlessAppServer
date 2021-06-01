67e00141cf6dcd2316548c8f232e43a78f2360ef
sungbum <shystyle@gmail.com>
2021-06-01 17:22:49 +0900

    feat : HotVideoVo 에 thumbnailImg 추가

--- src/main/java/com/fans/bravegirls/vo/model/HotVideoVo.java
+++ src/main/java/com/fans/bravegirls/vo/model/HotVideoVo.java
@@ -5,12 +5,19 @@ import lombok.Data;
 
 @Data
 public class HotVideoVo {
+
     private int id;
     private String code;
     private String title;
     private String playTime;
     private String uploadDate;
     private String channelTitle;
+
+
+    public String getThumbnailImg() {
+        return "https://img.youtube.com/vi/" + this.code + "/0.jpg";
+    }
+
 
     private List<HotVideoTagVo> tags;
 }
