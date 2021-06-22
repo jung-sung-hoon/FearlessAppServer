2f62355aabecff6a330c71805f7b80f68502c9b1
Red-Jung <applered80@gmail.com>
2021-06-22 23:35:56 +0900

    리턴 json null 을 '' 으로 변경

---
+++ src/main/java/com/fans/bravegirls/config/JsonCustomConfigure.java
@@ -0,0 +1,32 @@
+package com.fans.bravegirls.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+
+@Configuration
+public class JsonCustomConfigure {
+
+    @Bean
+    public MappingJackson2HttpMessageConverter converter() {
+
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        CustomObjectMapper mapper = new CustomObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        converter.setObjectMapper(mapper);
+
+        return converter;
+
+    }
+
+}
+
+
+
+
+
+
+
